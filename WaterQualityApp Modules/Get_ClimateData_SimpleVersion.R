f66a3e13fae1cc94d850ad7c730e50d5dfe391d4
Saeed Rahimi <112530122+sa-rahimi@users.noreply.github.com>
2023-07-27 20:16:14 +0900

    Update Get_ClimateData_SimpleVersion.R

--- WaterQualityApp Modules/Get_ClimateData_SimpleVersion.R
+++ WaterQualityApp Modules/Get_ClimateData_SimpleVersion.R
@@ -9,7 +9,7 @@ library(clifro)
 # Set up user account (This must be done on NIWA's website!)
 cur.user <- cf_user('saeed@landwatersci.net', 'UNR5KGBO')
 
-get_climate_data <- function(d.type, where, date) {
+get_climate_data <- function(id, d.type, where, date) {
   tryCatch({
     # Create a lookup table for data types
     data.type.lookup <- list(
@@ -48,13 +48,13 @@ get_climate_data <- function(d.type, where, date) {
            '\n Please try another location or increase the radius')
     } else {
       dat <- data.frame()
-      ## Define the number of rows in each smaller list
-      rows.per.list <- 20
+      ## Define the number of rows in each smaller list of stations
+      rows.per.list <- 10
       
-      ## Calculate the number of smaller lists needed
+      ## Calculate the number of smaller lists
       num.lists <- ceiling(nrow(stations.list) / rows.per.list)
       
-      #' Split the stations list into smaller lists with 20 stations at max.
+      #' Split the stations list into smaller lists with 10 stations at max.
       #' This is because of the limitation set by the package
       smaller.st.lists <- lapply(seq_len(num.lists), function(i) {
         start.row <- (i - 1) * rows.per.list + 1
@@ -95,6 +95,13 @@ get_climate_data <- function(d.type, where, date) {
       if (nrow(dat) > 0) {
         break
       }
+      
+      # # Save the retrived climate data in a csv
+      # file_name <- paste("Report/", id, "_", date[2], "_", d.type, ".csv", sep = "")
+      # file_dir <- paste(out.f.dir, file_name, sep = "")
+      # 
+      # # Save the data.frame to a CSV file with the constructed file name
+      # write.csv(dat, file = file_dir, row.names = FALSE)
       
     }, error = function(e) {
       # Return a value indicating the error condition (optional)
@@ -166,8 +173,11 @@ library(reticulate)
 library(lubridate)
 library(tidyr)
 
+# Assign the data directory
+out.f.dir = "C:/Users/SaeedR/Desktop/Saeed/App/Data/Climate/"
+
 # Set the file path for the .p (pickle) file
-in.f.dir <- 'D:/OneDrive/Work/Imaginative AI/Projects/WaterQualityApp/Data/WQ_Data_Nov2021.p'
+in.f.dir <- 'C:/Users/SaeedR/Desktop/Saeed/App/Data/WQ_Data_Nov2021.p'
 
 # Use reticulate to read the pickle data
 dat.dic <- py_load_object(in.f.dir)
@@ -184,14 +194,15 @@ filtered.metadata <- metadata %>%
   filter(nzsegment %in% filtered.stackframe$nzsegment)
 
 # Create a lookup table for the parameter names
-climate.parameters <- c("wind", "rain", "air.temp", "earth.temp", "soil.moist")
+climate.parameters <- c("rain", "air.temp", "earth.temp", "soil.moist") # "wind",
 
 # Create an empty list that will contain all the climate data for all sampling 
 # sites over the sampling periods
 climate.dat.list <- list()
 
 # Loop over the sampling sites list
-for (i in 1:nrow(filtered.metadata[1:2, ])) {
+# for (i in 1:nrow(filtered.metadata[1:2, ])) {
+for (i in 1:nrow(filtered.metadata)) {
   seg.id <- filtered.metadata$nzsegment[i]
   lat <- filtered.metadata$lat[i]
   lon <- filtered.metadata$long[i]
@@ -201,6 +212,12 @@ for (i in 1:nrow(filtered.metadata[1:2, ])) {
   # Get the sampling dates for the current sampling site
   sampeling.dates <- filtered.stackframe$myDate[filtered.stackframe$nzsegment == seg.id] %>%
     unique()
+  
+  # Convert the list to a vector
+  date_vector <- sapply(sampeling.dates, as.character)
+  
+  # Get the unique values
+  sampeling.dates <- unique(date_vector)
 
   # Create an empty data frame that will contain all the climate data for a
   # specific sampling site over all sampling periods
@@ -208,19 +225,28 @@ for (i in 1:nrow(filtered.metadata[1:2, ])) {
   seg.climate.dat <- data.frame(SegID = character(), Date = character(), Parameter = character(), Val = numeric())
 
   # Loop over the sampling dates for the current sampling site
-  for (date in sampeling.dates[1:2]) {
+  # for (date in sampeling.dates[1:2]) {
+  for (date in sampeling.dates) {
     # reformat the sampling date to string
-    end.date <- format(date, "%Y-%m-%d")
+    end.date <- date #format(date, "%Y-%m-%d")
 
     # Subtract 1 day from the sampling date
     start.date <- as.Date(end.date) - 1
 
     # Loop over the list of climate data
-    for (data.type in climate.parameters[1:2]) {
+    # for (data.type in climate.parameters[1:2]) {
+    for (data.type in climate.parameters) {
       # Request the historical climate data from NIWA
-      climate.dat <- get_climate_data(data.type, c(lat, lon, 100), 
+      climate.dat <- get_climate_data(seg.id, data.type, c(lat, lon, 100), 
                                       c(start.date, end.date))
-
+      
+      # Save the retrived climate data in a csv
+      file_name <- paste("Processed/", i, "_", seg.id, "_", date, "_", data.type, ".csv", sep = "")
+      file_dir <- paste(out.f.dir, file_name, sep = "")
+      
+      # Save the data.frame to a CSV file with the constructed file name
+      write.csv(climate.dat, file = file_dir, row.names = FALSE)
+      
       # Call the function to calculate the IDW mean of the "Percent..." column
       idw.mean.val <- get_idw_mean(data=climate.dat, distance.col="distance", 
                                    d.type=data.type)
